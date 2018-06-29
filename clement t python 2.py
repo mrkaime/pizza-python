5c6a5f2a5aa0addce8001c031818f0649da7b470
mrkaime <clement.toni6@gmail.com>
2018-06-29 13:04:18 +1200

    pizza

---
+++ clement t python 2.py
@@ -0,0 +1,14 @@
+meat = 10
+vegetable = 10
+renne = 10
+
+print ("welcome to... what do you whant ?")
+print ("meat","vegetable","renne")
+
+if ("meat"):#'vegetable','renne']
+    print ("do you whant an other pizza ?[y/n]") 
+elif ('y'):
+    print ("meat","vegetable","renne")
+
+elif ('n'):
+    print ("enter your adress /nenter yout phone number")
